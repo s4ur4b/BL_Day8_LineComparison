a822e38eb36e9b84690cab002c168a57273e24d8
s4ur4b <saurabhchavn2110@gmail.com>
2021-08-20 06:39:31 +0530

    Added:Display Message

---
+++ LineComparison.java
@@ -0,0 +1,7 @@
+package comparison;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("This is a program to compare Line Lengths");
+    }
+}
